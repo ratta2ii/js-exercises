e46bff975c5474f24ea0d51ea301027203465164
Trevor <tismetrm@gmail.com>
2019-10-21 21:29:58 -0700

    It will demostrate using funcitons

---
+++ functions.js
@@ -0,0 +1,23 @@
+let answer = prompt("Enter a lowercase sentence here:");
+
+
+let capitalizeFandL = function(answer) {
+  let first = answer[0].toUpperCase();
+  let last = answer[answer.length - 2].toUpperCase();
+  return first + last;
+}
+
+let lettersToReverse = capitalizeFandL(answer);
+console.log(lettersToReverse);
+
+let reverseLetters = function(lettersToReverse){
+  return lettersToReverse.split("").reverse().join("");
+}
+
+var appendingLetters = reverseLetters(lettersToReverse);
+
+var concatSentenceAndLetters = function() {
+  return answer + appendingLetters;
+}
+
+alert(concatSentenceAndLetters());
